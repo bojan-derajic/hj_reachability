2b627573aa0ede098fec44d746fa31b3de88f1c8
Bojan Derajic <derajicbojan@gmail.com>
2025-11-07 08:14:57 +0000

    Implemented model for the 'invert pendulum' system

--- hj_reachability/systems/__init__.py
+++ hj_reachability/systems/__init__.py
@@ -2,6 +2,7 @@ from hj_reachability.systems.air3d import Air3d, DubinsCarCAvoid
 from hj_reachability.systems.double_integrator_1d import DoubleIntegrator1D
 from hj_reachability.systems.double_integrator_2d import DoubleIntegrator2D
 from hj_reachability.systems.kinematic_unicycle import KinematicUnicycle
+from hj_reachability.systems.inverted_pendulum import InvertedPendulum
 
 __all__ = [
     "Air3d",
@@ -9,4 +10,5 @@ __all__ = [
     "DoubleIntegrator1D",
     "DoubleIntegrator2D",
     "KinematicUnicycle",
+    "InvertedPendulum",
 ]
---
+++ hj_reachability/systems/inverted_pendulum.py
@@ -0,0 +1,39 @@
+import jax.numpy as jnp
+from typing import Literal
+
+from hj_reachability import dynamics, sets
+
+
+class InvertedPendulum(dynamics.ControlAndDisturbanceAffineDynamics):
+    """Class that implements inverted pendulum model."""
+
+    m = 2.0  # mass of the pendulum
+    l = 1.0  # length of the pendulum
+    g = 9.81  # acceleration due to gravity
+
+    def __init__(
+        self,
+        control_mode: Literal["min", "max"] = "max",
+        disturbance_mode: Literal["min", "max"] = "min",
+        control_space=None,
+        disturbance_space=None,
+    ):
+        if control_space == None:
+            control_space = sets.Box(lo=jnp.array([0.0]), hi=jnp.array([0.0]))
+        if disturbance_space == None:
+            disturbance_space = sets.Box(lo=jnp.array([0.0]), hi=jnp.array([0.0]))
+        super().__init__(
+            control_mode, disturbance_mode, control_space, disturbance_space
+        )
+
+    def open_loop_dynamics(self, state, time):
+        """Implements open loop dynamics"""
+        return jnp.array([[state[1]], [(self.g / self.l) * jnp.sin(state[0])]])
+
+    def control_jacobian(self, state, time):
+        """Calculates control Jacobian"""
+        return jnp.array([[0.0], [1.0 / (self.m * self.l**2)]])
+
+    def disturbance_jacobian(self, state, time):
+        """Calculates disturbance Jacobian"""
+        return jnp.array([[0.0], [1.0]])
